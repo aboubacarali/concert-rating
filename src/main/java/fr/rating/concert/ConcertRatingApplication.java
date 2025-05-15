a61ebacc90a04f3b29db9bd1579e163140976f4e
aboubacarali <aboubacarali532@gmail.com>
2025-05-15 11:13:47 +0200

    First commit

---
+++ src/main/java/fr/rating/concert/ConcertRatingApplication.java
@@ -0,0 +1,13 @@
+package fr.rating.concert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertRatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertRatingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/rating/concert/ConcertRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.rating.concert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertRatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
